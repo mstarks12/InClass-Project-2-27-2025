31e869d7b3365751dcf78c5a3a69f787c838335b
Marc Starks <starksmr@mail.uc.edu>
2025-03-27 11:51:23 -0400

    Add project files.

---
+++ InClass Project 2-27-2025/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_Project_2_27_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ InClass Project 2-27-2025/WriteHello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_Project_2_27_2025
+{
+    internal class WriteHello
+    {
+        private String _hello;
+
+        public string Hello { get { return _hello; } set { _hello = value; } }
+    }
+}
